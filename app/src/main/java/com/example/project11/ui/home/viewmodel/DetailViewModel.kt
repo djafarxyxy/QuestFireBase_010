816422529606a49e68f7ee9f522321502a8238e8
djafarxyxy <farhanch12344@gmail.com>
2025-01-15 17:00:20 +0700

    menambahkan judulSkripsi dll

---
+++ app/src/main/java/com/example/project11/ui/home/viewmodel/DetailViewModel.kt
@@ -0,0 +1,82 @@
+package com.example.project11.ui.home.viewmodel
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project11.model.Mahasiswa
+import com.example.project11.navigation.DestinasiNavigasi
+import com.example.project11.repository.NetworkRepositoryMhs
+import com.example.project11.ui.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+class DetailViewModel(private val mhsRepository: NetworkRepositoryMhs) : ViewModel() {
+
+    var uiState by mutableStateOf(DetailUiState())
+        private set
+
+    fun fetchDetailMahasiswa(nim: String) {
+        viewModelScope.launch {
+            uiState = DetailUiState(isLoading = true)
+            try {
+                val mahasiswa = mhsRepository.getMahasiswaById(nim)
+                uiState = DetailUiState(detailUiEvent = mahasiswa.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+}
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiState = InsertUiState(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiState()
+}
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiState {
+    return InsertUiState(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan,
+        JudulSkripsi = JudulSkripsi,
+        DosenPembimbing1 = DosenPembimbing1,
+        DosenPembimbing2 = DosenPembimbing2
+
+    )
+}
